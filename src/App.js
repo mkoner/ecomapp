1431fb5eca7629bf5b8e7d5201ff6e7fee33eb22
m koner <mdoukoner@gmail.com>
2021-06-02 20:30:36 +0530

    adding shop data to firebase

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends React.Component {
       setCurrentUser(userAuth);
       addCollectionAndDocuments('collections', 
       collectionsArray.map(({title,items}) => ({title,items})) );
+
     });
   }
 
